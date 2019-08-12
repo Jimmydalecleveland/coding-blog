9992aaa114ecfa9d47e670c1961c138cda8a8154
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2019-08-11 22:10:15 -0600

    Color updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,15 +24,13 @@ class BlogIndex extends React.Component {
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
-            <div key={node.fields.slug}>
+            <div key={node.fields.slug} style={{ marginBottom: '3rem' }}>
               <h3
                 style={{
                   marginBottom: rhythm(1 / 4),
                 }}
               >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
+                <Link to={node.fields.slug}>{title}</Link>
               </h3>
               <small>{node.frontmatter.date}</small>
               <p
