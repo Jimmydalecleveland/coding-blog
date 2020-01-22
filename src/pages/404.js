15a29f008db0f807ab76cf03daa69c318543496c
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2020-01-22 13:58:15 -0700

    mess things up

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,11 @@ import Layout from '../components/Layout'
 
 class NotFoundPage extends React.Component {
   render() {
+    let temp
+    for (let i = 0; i < 100000; i++) {
+      temp = i
+    }
+
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const siteDescription = data.site.siteMetadata.description
@@ -15,10 +20,10 @@ class NotFoundPage extends React.Component {
         <Helmet
           htmlAttributes={{ lang: 'en' }}
           meta={[{ name: 'description', content: siteDescription }]}
-          title={siteTitle}
         />
         <h1>Not Found</h1>
         <p>The page in your URL bar just cannot be found, despite our best efforts.</p>
+        <h3>temp</h3>
         <img src="https://w.wallhaven.cc/full/83/wallhaven-836q9j.jpg" />
       </Layout>
     )
