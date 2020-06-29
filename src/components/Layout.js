5d7462acbafff13c11518cfd487a70ea8aa32423
Jimmy DC <jimmydalecleveland@gmail.com>
2020-06-28 21:15:48 -0600

    add sword logo and fly in animation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import sword from '../assets/images/sword.svg'
 
 // Import typefaces
 import 'typeface-crimson-text'
@@ -10,32 +11,46 @@ import 'typeface-fira-sans'
 import { rhythm, scale } from '../utils/typography'
 import Sidebar from './Sidebar'
 
+const queryParamsFjord = /\?source=parallaxfjord/
+
 const Layout = props => {
   const data = useStaticQuery(BACKROUND_IMAGE_QUERY)
   const { location, title, children } = props
   const rootPath = `${__PATH_PREFIX__}/`
   let header
 
+
   if (location.pathname === rootPath) {
     header = (
-      <h1
-        style={{
-          ...scale(1.4),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
+      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+        <h1
           style={{
-            boxShadow: 'none',
-            textDecoration: 'none',
-            color: 'inherit',
+            ...scale(1.4),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+            flex: '0 0 40px',
           }}
-          to={'/'}
         >
-          {title}
-        </Link>
-      </h1>
+          <Link
+            style={{
+              boxShadow: 'none',
+              textDecoration: 'none',
+              color: 'inherit',
+            }}
+            to={'/'}
+          >
+            {title}
+          </Link>
+        </h1>
+        <div className="sword-wrapper"
+          style={{
+            transform: queryParamsFjord.test(location.search) ? 'translate(150px, -270px)' : '',
+            animation: queryParamsFjord.test(location.search) ? '0.5s cubic-bezier(.76,.05,.86,.06) 1s 1 normal forwards running swordFlyIn' : '',
+          }}
+        >
+          <img src={sword} alt="swashbuckling sword" style={{ width: '40px', transform: 'rotate(20deg)', flex: '0 0 40px' }} />
+        </div>
+      </div>
     )
   } else {
     header = (
